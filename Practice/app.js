d1a7ae55fd2fd7e49d0cd7f4c414ef28b97b28a1
ankur.p <patel.ankur2979@gmail.com>
2025-09-02 12:35:32 +0530

    video 5 code

--- Practice/app.js
+++ Practice/app.js
@@ -5,5 +5,17 @@
 // var fs = require('fs');
 // fs.writeFileSync("data.txt","This is test description");
 
-// vid-04
+// vid-05
 
+// import value from other file
+
+// Option 1
+
+// const data = require("./data");
+// console.log(data);
+// console.log(data.userName);
+
+// Option 2
+
+// const {userName}  = require("./data");
+// console.log(userName);
---
+++ Practice/data.js
@@ -0,0 +1,14 @@
+// export variables example
+
+// Option 1
+
+// export let userName = "akp";   
+
+// export let userCity = "chikhli";   
+
+// Option 2
+
+// module.exports={
+//     userName : "akp",
+//     userCity : "chikhli"   
+// }
