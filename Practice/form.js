9e52497c2cb0581f9785092819d753cd7049b4a6
ankur.p <patel.ankur2979@gmail.com>
2025-09-03 00:35:53 +0530

    get form data and display submitted data

--- Practice/form.js
+++ Practice/form.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 http.createServer((req, res) => {
     fs.readFile('html/web.html', 'utf-8', (err, data) => {
@@ -12,12 +13,24 @@ http.createServer((req, res) => {
 
         if (req.url == '/') {
             res.writeHead(200, { "content-type": 'text/html' });
-            res.write(data);            
+            res.write(data);
+            res.end();
         } else if (req.url == '/submit') {
-            res.write("Data Submitted");
+            let bodyData = [];
+            req.on('data', (chunk) => {
+                bodyData.push(chunk);
+            });
+
+            req.on('end', () => {
+                let rawData = Buffer.concat(bodyData).toString();
+                let readableData = querystring.parse(rawData);
+                // console.log(readableData);
+                res.writeHead(200, { "content-type": 'text/html' });
+                res.write("<h3>Data Submitted</h3>");
+                res.write("<p>Username : " + readableData.username + "</p>");
+                res.end();
+            })
         }
-        
-        res.end();
 
     })
 
