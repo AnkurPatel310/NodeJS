c18d32fb3220d2b845f4219a9d97a2951941850c
ankur.p <patel.ankur2979@gmail.com>
2025-09-05 20:07:09 +0530

    render html file and 404 page

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -5,11 +5,19 @@ import home from './pages/home.js';
 import about from './pages/about.js';
 import { signup } from './pages/signup.js';
 import { formSubmit } from './pages/formSubmit.js';
+import path from 'path';
+import { viewDir } from './path.js';
 
 const app = express();
 
 app.get("",(req,res)=>{
-    res.send(home());
+    // res.send(home());
+
+    res.sendFile(path.resolve('view/home.html'));
+});
+
+app.get("/login",(req,res)=>{   
+    res.sendFile(path.resolve('view/login.html'));
 });
 
 app.get("/about",(req,res)=>{
@@ -22,6 +30,10 @@ app.get("/signup",(req,res)=>{
 
 app.post("/submit",(req,res)=>{
     res.send(formSubmit());
+});
+
+app.use((req, res)=>{
+    res.status(404).sendFile(viewDir+"/404.html");
 });
 
 app.listen(6100);
