fa6cf7ad87e0f880aeea6329f16c2218b4756ed5
ankur.p <patel.ankur2979@gmail.com>
2025-09-10 16:08:13 +0530

    POST REST API - Mongoose

--- REST-Mongoose/index.js
+++ REST-Mongoose/index.js
@@ -10,13 +10,26 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-await mongoose.connect(url).then(()=>{
+await mongoose.connect(url).then(() => {
     console.log('db connected');
 })
 
-app.get('/',async (req,res)=>{
+// GET API
+app.get('/', async (req, res) => {
     const data = await studentModel.find();
     res.status(200).send(data);
+})
+
+// POST API
+app.post('/save', async (req, res) => {
+    const { name, grade } = req.body;
+    if (!name || !grade) {
+        res.send({ message: 'Insert Failed', result: false, data: null })
+        return false;
+    } else {
+        const studentData = studentModel.create(req.body);
+        res.send({ message: 'Data Inserted', result: true, data: req.body })
+    }
 })
 
 app.listen(6100);
